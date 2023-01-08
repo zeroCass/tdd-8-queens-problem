a3bd39734a114a83dced17152fe0a5b47431d140
Mateus Valerio <mateusvalerio1@gmail.com>
2023-01-08 13:35:04 -0300

    second-test-fails: test if a board input is valid
    the function is_valid_board will check if the paramater board has 8x8 size. Returns true if it is and False if is not

--- eight_queens.py
+++ eight_queens.py
@@ -3,6 +3,20 @@ This program check if a input 8x8 list that represents a chessboard
 is a solution for eight queens problem
 """
 
+def is_valid_board(board: list) -> bool:
+    """ check if the board has size of 8x8 """
+    # check if the board has 8 rows
+    if len(board) != 8:
+        return False
+
+    # check if the board has 8 columns
+    for row in board:
+        if len(row) != 8:
+            return False
+    return True
+
 def is_solution_to_8_queens_problem(board) -> int:
     """ check the input it is a soluction for the 8 queens problem """
+    if not is_valid_board(board):
+        return -1
     return 1
--- test_eight_queens.py
+++ test_eight_queens.py
@@ -1,7 +1,13 @@
 from eight_queens import *
 
 def test_solution_function_exists():
-    """ function that  test if is_solution_to_8_queens_problem exists """
+    """ function that  test if is_solution_to_8_queens_problem exists """  
     result = is_solution_to_8_queens_problem(board = [])
     assert isinstance(result, int) is True
 
+def test_check_if_is_valid_board():
+    """ this function will test if a board input has size of 8 x 8
+    the result should be -1 if is not and 1 if it is """
+
+    result = is_valid_board(board=[])
+    assert result == True
